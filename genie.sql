b4413b12bf2470b1f9810b56777c05f8bfcef06c
Gerald Ding <geralddzx@gmail.com>
2020-03-11 10:58:46 -0400

    primary key

--- genie.sql
+++ genie.sql
@@ -2,9 +2,6 @@ CREATE DATABASE genie;
 
 \c genie;
 
-DROP TABLE public.articles;
-DROP TABLE public.entities;
-
 CREATE TABLE public.articles(
   id character varying PRIMARY KEY NOT NULL,
   filename character varying NOT NULL,
@@ -14,13 +11,12 @@ CREATE TABLE public.articles(
   updated_at timestamp without time zone NOT NULL
 );
 
-CREATE UNIQUE INDEX index_articles_on_id ON public.articles USING btree (id);
 CREATE INDEX index_articles_on_published_at ON public.articles USING btree (published_at);
 CREATE INDEX index_articles_on_processed ON public.articles USING btree (processed);
 
 CREATE TABLE public.entities(
-  id character varying PRIMARY KEY NOT NULL,
-  count integer NOT NULL
+  name character varying NOT NULL,
+  year integer NOT NULL,
+  count integer NOT NULL DEFAULT 0,
+  PRIMARY KEY(name, year)
 );
-
-CREATE UNIQUE INDEX index_entities_on_id ON public.entities USING btree (id);

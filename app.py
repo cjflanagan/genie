0fc163772689da5a9be652a4fdbdbd798277b8f1
Gerald Ding <geralddzx@gmail.com>
2020-03-13 04:44:44 -0400

    fix

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def entities():
             ents_list = []
             for ent in ents:
                 entity = ents[ent]
-                ents_list.append((ent, entity[0], entity[1], (entity[1][-1] - entity[1][0]) ** 2), statistics.variance(entity[1]))
+                ents_list.append((ent, entity[0], entity[1], (entity[1][-1] - entity[1][0]) ** 2, statistics.variance(entity[1]) if len(entity[1]) > 1 else 0))
 
             ents_list = sorted(ents_list, reverse = True, key = itemgetter(4))[:50]
             return jsonify(ents_list)

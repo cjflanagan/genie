44c55837a03281178c6e8d59321e68634992ba94
Gerald Ding <geralddzx@gmail.com>
2020-03-14 02:51:05 -0400

    visualization

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import datetime
 import pdb
 import statistics
 from operator import itemgetter
+import numpy as np
 
 app = Flask("genie")
 
@@ -27,6 +28,8 @@ def entities():
             year_counts = {}
             for year_count in cur.fetchall():
                 year_counts[int(year_count[0])] = year_count[1]
+            years = sorted([*year_counts])
+
 
             cur.execute("""
                 SELECT name, year, count
@@ -39,18 +42,40 @@ def entities():
                     LIMIT 500)
                 ORDER BY year;
             """)
-            ents = {}
+            entity_counts = {}
             for entity in cur.fetchall():
-                ents.setdefault(entity[0], ([], []))
-                ents[entity[0]][0].append(entity[1])
-                ents[entity[0]][1].append(entity[2] / float(year_counts[entity[1]]))
+                entity_counts.setdefault(entity[0], {})
+                entity_counts[entity[0]][entity[1]] = float(entity[2]) / year_counts[entity[1]]
 
-            ents_list = []
-            for ent in ents:
-                entity = ents[ent]
-                ents_list.append((ent, entity[0], entity[1], (entity[1][-1] - entity[1][0]) ** 2, statistics.variance(entity[1]) if len(entity[1]) > 1 else 0))
+            entity_measures = []
+            measure_types = ["change", "relative change", "average change", "current", "variance", "std", "mean", "relative std"]
+            for ent in entity_counts:
+                entity_count = entity_counts[ent]
+                data = []
+                counts = []
+                for year in years:
+                    if year in entity_count:
+                        data.append(round(entity_count[year], 2))
+                        counts.append(entity_count[year])
+                    else:
+                        data.append(None)
 
-            ents_list = sorted(ents_list, reverse = True, key = itemgetter(3))[:50]
-            return jsonify(ents_list)
+                y = np.array(counts)
+                entity_measures.append((ent, data, y[-1] - y[0], y[-1] / y[0], np.mean(y[1:] / y[0:-1]), y[-1], np.var(y), np.std(y), np.mean(y), np.std(y) / np.mean(y)))
+
+            entity_lists = []
+            for i, type in enumerate(measure_types):
+                for b in range(2):
+                    entity_list = {
+                        "type": type,
+                        "desc": not b,
+                        "years": years,
+                        "data": []
+                    }
+                    sorted_list = sorted(entity_measures, reverse = not b, key = itemgetter(i + 2))[:20]
+                    for entity in sorted_list:
+                        entity_list["data"].append((entity[0], entity[1], round(entity[i + 2], 2)))
+                    entity_lists.append(entity_list)
+            return jsonify(entity_lists)
 
 app.run()
